f7fba8cb3f1da16d1cb034a6e299f502ea1972d5
Liang Xie <xieliaing@users.noreply.github.com>
2025-07-06 21:10:49 +0900

    Update YouTubeDownloader2.py
    adding command line parameter parsing for custom video urls.

--- Side-Project/Youtube/YouTubeDownloader2.py
+++ Side-Project/Youtube/YouTubeDownloader2.py
@@ -1,6 +1,25 @@
+#created with the help of ChatGPT on 4/15/2024
+# update 7/5/2025: added command line parameter parser
+#
 import yt_dlp
+import argparse
+import os
 
-video_url = "https://youtu.be/Ej7SK6aQobs?si=nEc6kbLWJHxhEjBQ"
+
+
+# Define command line arguments
+parser = argparse.ArgumentParser(description='Download and convert a YouTube video to MP3.')
+parser.add_argument('url', type=str, help='The YouTube video URL.')
+parser.add_argument('--output-dir', type=str, default='./', help='The output directory for the MP3 file.')
+
+# Parse command line arguments
+args = parser.parse_args()
+
+print(args.url)
+print(args.output_dir)
+
+video_url = args.url
+#video_url = "https://youtu.be/Ej7SK6aQobs?si=nEc6kbLWJHxhEjBQ"
 
 ydl_opts = {
     'format': 'bestvideo+bestaudio/best',  # Get best quality
