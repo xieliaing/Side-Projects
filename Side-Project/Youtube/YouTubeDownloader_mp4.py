f13fcc9dbe4a702db31542f9819bc4b323a1eb25
Liang Xie <xieliaing@users.noreply.github.com>
2025-12-27 00:21:19 -0800

    only download mp4 format
    mp4 is the most compatible with iPad, tested on iPad Pro 11th gen, iPhone 15 Pro Max, iPhone 12 Pro Max

---
+++ Side-Project/Youtube/YouTubeDownloader_mp4.py
@@ -0,0 +1,30 @@
+#created with the help of ChatGPT on 4/15/2024
+# update 7/5/2025: added command line parameter parser
+#
+import yt_dlp
+import argparse
+import os
+
+
+
+# Define command line arguments
+parser = argparse.ArgumentParser(description='Download YouTube video as MP4 file.')
+parser.add_argument('url', type=str, help='The YouTube video URL.')
+parser.add_argument('--output-dir', type=str, default='./', help='The output directory for the MP4 file.')
+
+# Parse command line arguments
+args = parser.parse_args()
+
+print(args.url)
+print(args.output_dir)
+
+video_url = args.url
+#video_url = "https://youtu.be/Ej7SK6aQobs?si=nEc6kbLWJHxhEjBQ"
+
+ydl_opts = {
+    'format': 'best[ext=mp4]/best',  # Get best quality
+    'outtmpl': '%(title)s.%(ext)s'         # Save with title name
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([video_url])
